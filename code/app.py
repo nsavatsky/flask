82b1d208480ead192b43646063ac3b8c0b3acd29
nicol <savatsky18@gmail.com>
2022-08-13 21:15:27 -0300

    refresh2

--- code/app.py
+++ code/app.py
@@ -6,7 +6,6 @@ from security import authenticate_user, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
     
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -16,11 +15,9 @@ app.secret_key = 'bob'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
-db.init_app(app)
+
+
 jwt = JWT(app, authenticate_user, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
